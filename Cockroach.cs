90d95b4abe174c9b4340369efbe62b273b9712d1
tojarr <30999615+tojarr@users.noreply.github.com>
2019-01-26 13:39:24 +0200

    Created class Cockroach

---
+++ Cockroach.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CockroachRaceV2
+{
+    public class Cockroach
+    {
+        string Name { get; set; }
+        int Speed { get; set; }
+        int Distance  { get; }
+        int count = 0;
+        //public int time;
+        Random rnd = new Random(DateTime.Now.Millisecond);
+
+        public Cockroach(int n)
+        {
+            Name = "Bug" + n;
+            Distance = 0;
+            Speed = (rnd.Next(1, 5));
+        }
+
+        public void BugRun()
+        {
+        }
+    }
+}
