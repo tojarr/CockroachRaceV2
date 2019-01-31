84b40b59f76ff6d9042bddf546531917fa9f5942
tojarr <30999615+tojarr@users.noreply.github.com>
2019-01-31 23:56:06 +0200

    Add logic,return class InitOfCockroaches

--- Cockroach.cs
+++ Cockroach.cs
@@ -11,25 +11,89 @@ namespace CockroachRaceV2
     {
         public string Name { get; set; }
         int Speed { get; set; }
-        public int Distance  { get; }
+        int distance = 0;
+        int Snapshot { get; set; }
         int count = 0;
-        //public int time;
-        Random rnd = new Random(DateTime.Now.Millisecond);
+        int Distance
+        {
+            get
+            {
+                return distance;
+            }
+        }
+
+        public Cockroach()
+        {
+
+        }
 
         public Cockroach(int n)
         {
-            Name = "Bug" + n;
-            Distance = 0;
-            Speed = (rnd.Next(1, 5));
+            Name = "Bag" + n;
         }
 
-        public void BugRun()
+        void BugRun(object obj)
         {
+            Speed = Rand();
+            distance += Speed;
+        }
+
+        public void MyTimer(Cockroach cockroach)
+        {
+            TimerCallback tcb = new TimerCallback(cockroach.BugRun);
+            var timer = new Timer(tcb, 1000, 0, 1000);
+
+        }
+
+        int Rand()
+        {
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            return rnd.Next(1, 5);
+        }
+
+        List<Cockroach> SortResult()
+        {
+            var list = Cockroaches.AllCockroaches.OrderBy(x => x.Distance);
+            return list.ToList();
+        }
+
+        void PrintResult(object obj)
+        {
+            ForPrintResult forPrint = (ForPrintResult)obj;
+            if (count == (forPrint.TimeOut / 1000))
+            {
+                Console.WriteLine("Round stop");
+                count++;
+            }
+            else if (count < (forPrint.TimeOut / 1000))
+            {
+                SortResult().ForEach(x => Console.WriteLine($"Name: {x.Name} - distance(sm/sec): {x.Distance}"));
+                Console.WriteLine(new string('-', 35));
+                count += forPrint.Snapshot / 1000;
+            }
+            else
+            {
+                return;
+            }
+
+        }
+
+        public void TimerPrintResult(int snapshot, int timeOut)
+        {
+            TimerCallback tcb = new TimerCallback(PrintResult);
+            Timer timer = new Timer(tcb, new ForPrintResult { TimeOut = timeOut, Snapshot = snapshot }, snapshot, snapshot);
         }
     }
 
-    public class Cockroaches
+    public static class Cockroaches
     {
         public static List<Cockroach> AllCockroaches = new List<Cockroach>();
+    }
+
+    public class ForPrintResult
+    {
+        public int TimeOut { get; set; }
+        public int Snapshot { get; set; }
+        public Timer IsTimer { get; set; }
     }
 }
